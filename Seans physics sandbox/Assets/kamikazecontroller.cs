5673971642fc9a0c652f19cbd70f1646ffbc94c9
ROver213 <seanmahern@icloud.com>
2021-09-02 19:04:01 +1000

    flames has flames

---
+++ Seans physics sandbox/Assets/kamikazecontroller.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kamikazecontroller : MonoBehaviour
+{
+    public ParticleSystem engineparticle;
+
+
+
+    Rigidbody2D rb;
+    public float movespeed;
+    public float rotatespeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+      rb = GetComponent<Rigidbody2D>();
+      
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W)){
+          rb.AddRelativeForce(Vector2.up*movespeed);
+            if(engineparticle.isPlaying == false)
+            {
+                engineparticle.Play();
+            }
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            engineparticle.Stop();
+        }
+
+
+        if(Input.GetKey(KeyCode.D)){
+          transform.Rotate(0,0,-rotatespeed,Space.World);
+        }
+         if(Input.GetKey(KeyCode.A)){
+          transform.Rotate(0,0,rotatespeed,Space.World);
+        }
+
+    }
+}
